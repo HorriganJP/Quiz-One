15e46c9124bcd4e296bc19a6ba7373d1e3afce2f
JP Horrigan <Horrigan.JP@outlook.com>
2019-08-24 01:31:42 +0000

    Quiz Two Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,11 +14,15 @@ class ItemsController < ApplicationController
 
   def create
     Item.create(item_params)
+    
     redirect_to root_path
   end
 
-  def edit 
+  def update 
     @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to root_path
   end
 
   def destroy
