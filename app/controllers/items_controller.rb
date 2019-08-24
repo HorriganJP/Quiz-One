c4a825b5e71dd3ca3b5d5079d63e4ff40c831e83
JP Horrigan <Horrigan.JP@outlook.com>
2019-08-24 00:22:15 +0000

    Quiz One Complete

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,40 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  def edit 
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.delete
+      flash[:notice] = "Item deleted!"
+      redirect_to root_path
+    else
+      flash[:error] = "Failed to delete this item."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:task, :description, :due)
+  end
+end
